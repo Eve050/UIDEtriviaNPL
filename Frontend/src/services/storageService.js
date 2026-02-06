c30676a52b833d6a7de0a554b93766c615b843ca
Sebastián Chocho <sebas.a2035@gmail.com>
2026-02-06 09:50:02 -0500

    Update storageService.js

--- Frontend/src/services/storageService.js
+++ Frontend/src/services/storageService.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "uide-trivia-backend.vercel.app";
 
 /**
  * Guarda las preguntas generadas en el servidor backend
